2e8e65971c462e457c6fbb1f1852a83f1831f28c
jerrymusaga <musagatechnology@gmail.com>
2024-07-19 20:54:01 +0100

    create mint certificate struct that implements Account deserializer

--- programs/mint-nft-collection/src/lib.rs
+++ programs/mint-nft-collection/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, mint_to, Mint, TokenAccount, Token, MintTo};
 
 declare_id!("23vxUF22Q7pU5BJtFGwD23xZnF2dSfdCHQjuUbVize5J");
 
@@ -6,11 +7,20 @@ declare_id!("23vxUF22Q7pU5BJtFGwD23xZnF2dSfdCHQjuUbVize5J");
 pub mod mint_nft_collection {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn mint_certificate(ctx: Context<MintCertificate>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct MintCertificate<'info> {
+    #[account(mut)]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub token_account: Account<'info, TokenAccount>,
+    pub authority: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>
+}
