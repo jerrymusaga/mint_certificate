a2cdfceedff587c74387e6dfda20e65d0b90b334
jerrymusaga <musagatechnology@gmail.com>
2024-07-19 21:18:11 +0100

    instruction to mint a certificate

--- programs/mint-nft-collection/src/lib.rs
+++ programs/mint-nft-collection/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, mint_to, Mint, TokenAccount, Token, MintTo};
+use anchor_spl::token::{self, Mint, TokenAccount, Token, MintTo};
 
 declare_id!("23vxUF22Q7pU5BJtFGwD23xZnF2dSfdCHQjuUbVize5J");
 
@@ -7,8 +7,15 @@ declare_id!("23vxUF22Q7pU5BJtFGwD23xZnF2dSfdCHQjuUbVize5J");
 pub mod mint_nft_collection {
     use super::*;
 
-    pub fn mint_certificate(ctx: Context<MintCertificate>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn mint_certificate(ctx: Context<MintCertificate>, uri: String) -> Result<()> {
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info()
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::mint_to(cpi_ctx, 1)?;
         Ok(())
     }
 }
